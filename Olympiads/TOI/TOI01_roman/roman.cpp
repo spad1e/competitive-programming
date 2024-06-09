062a9beef509a5d6f53c1b94ace094aa48acdcac
spad1e <pasin.jechou@gmail.com>
2024-06-09 23:23:35 +0700

    toi01

---
+++ Olympiads/TOI/TOI01_roman/roman.cpp
@@ -0,0 +1,91 @@
+#pragma once
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef double db;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<ld, ld> pld;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<ld> vld;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+typedef vector<pld> vpld;
+typedef vector<vi> vvi;
+typedef tuple<ll, ll, ll> t3;
+typedef tuple<ll, ll, ll, ll> t4;
+typedef tuple<ll, ll, ll, ll, ll> t5;
+
+template<typename T> using pq = priority_queue<T>;
+template<typename T> using pqg = priority_queue<T, vector<T>, greater<T>>;
+
+#define rep0(a) for (int i = 0; i < a; ++i)
+#define rep1(i, a) for (int i = 0; i < a; ++i)
+#define rep2(i, a, b) for (int i = a; i <= b; ++i)
+#define rep3(i, a, b, c) for (int i = a; i <= b; i+=c) 
+#define overload4(a, b, c, d, e, ...) e
+#define rep(...) overload4(__VA_ARGS__, rep3, rep2, rep1, rep0)(__VA_ARGS__)
+#define repd0(a) for (int i = a; i >= 1; --i)
+#define repd1(i, a) for (int i = a; i >= 1; --i)
+#define repd2(i, a, b) for (int i = b; i >= a; --i)
+#define repd3(i, a, b, c) for (int i = b; i >= a; i-=c)
+#define repd(...) overload4(__VA_ARGS__, repd3, repd2, repd1, repd0)(__VA_ARGS__)
+#define trav(a, x) for (auto& a : x)
+
+#define sz(x) (int)(x).size()
+#define mp make_pair
+#define pb push_back
+#define st first
+#define nd second
+#define lb lower_bound
+#define ub upper_bound
+#define all(x) (x).begin(), (x).end()
+#define ins insert
+
+template<typename T> bool ckmin(T &a, const T &b) { return b < a ? a = b, 1 : 0; }
+template<typename T> bool ckmax(T &a, const T &b) { return a < b ? a = b, 1 : 0; }
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+const int MOD = 1e9 + 7;
+const int INF = 0x3fffffff;
+const ll LINF = 0x1fffffffffffffff;
+const char nl = '\n';
+const int MX = 1e4 + 10;
+
+int i, v, x, l, c;
+
+void solve() {
+  int n; cin >> n;
+  function<void(int)> count_roman = [&](int n) {
+    int k = n%10;
+    if (k <= 3) i += k;
+    else if (6 <= k && k <= 8) i += k-5;
+    if (4 <= k && k <= 8) v++;
+    if (k == 4) i++;
+    if (k == 9) i++, x++;
+
+    k = (n/10)%10;
+    if (k <= 3) x += k;
+    else if (6 <= k && k <= 8) x += k-5;
+    if (4 <= k && k <= 8) l++;
+    if (k == 4) x++;
+    if (k == 9) x++, c++;
+
+    k = (n/100)%10;
+    c += k;
+  };
+  rep(i, 1, n) count_roman(i);
+  cout << i << " " << v << " " << x << " " << l << " " << c << nl;
+}  
+
+int main(int argc, char* argv[]) {
+  ios_base::sync_with_stdio(0); cin.tie(NULL);
+  int t = 1;
+//  cin >> t;
+  while (t--) { solve(); }
+  return 0;
+}
