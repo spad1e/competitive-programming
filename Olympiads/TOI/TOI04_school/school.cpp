1a4331d5b5c924c580ea0f6b73ea17d8d483a299
spad1e <pasin.jechou@gmail.com>
2024-06-10 16:21:15 +0700

    toi04: school

---
+++ Olympiads/TOI/TOI04_school/school.cpp
@@ -0,0 +1,98 @@
+#pragma once
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef double db;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<ld, ld> pld;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<ld> vld;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+typedef vector<pld> vpld;
+typedef vector<vi> vvi;
+typedef tuple<ll, ll, ll> t3;
+typedef tuple<ll, ll, ll, ll> t4;
+typedef tuple<ll, ll, ll, ll, ll> t5;
+
+template<typename T> using pq = priority_queue<T>;
+template<typename T> using pqg = priority_queue<T, vector<T>, greater<T>>;
+
+#define rep0(a) for (int i = 0; i < a; ++i)
+#define rep1(i, a) for (int i = 0; i < a; ++i)
+#define rep2(i, a, b) for (int i = a; i <= b; ++i)
+#define rep3(i, a, b, c) for (int i = a; i <= b; i+=c) 
+#define overload4(a, b, c, d, e, ...) e
+#define rep(...) overload4(__VA_ARGS__, rep3, rep2, rep1, rep0)(__VA_ARGS__)
+#define repd0(a) for (int i = a; i >= 1; --i)
+#define repd1(i, a) for (int i = a; i >= 1; --i)
+#define repd2(i, a, b) for (int i = b; i >= a; --i)
+#define repd3(i, a, b, c) for (int i = b; i >= a; i-=c)
+#define repd(...) overload4(__VA_ARGS__, repd3, repd2, repd1, repd0)(__VA_ARGS__)
+#define trav(a, x) for (auto& a : x)
+
+#define sz(x) (int)(x).size()
+#define mp make_pair
+#define pb push_back
+#define st first
+#define nd second
+#define lb lower_bound
+#define ub upper_bound
+#define all(x) (x).begin(), (x).end()
+#define ins insert
+
+template<typename T> bool ckmin(T &a, const T &b) { return b < a ? a = b, 1 : 0; }
+template<typename T> bool ckmax(T &a, const T &b) { return a < b ? a = b, 1 : 0; }
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+const int MOD = 1e9 + 7;
+const int INF = 0x3fffffff;
+const ll LINF = 0x1fffffffffffffff;
+const char nl = '\n';
+const int MX = 2e3 + 10;
+
+char school[MX][MX];
+int dp[MX][MX], qsP[MX][MX], qsT[MX][MX], dx[] = {0, 0, 1, -1}, dy[] = {1, -1, 0, 0};
+bool mark[MX][MX];
+
+void flood(int x, int y) {
+  mark[x][y] = 1;
+  rep(i, 4) {
+    int xx = x+dx[i], yy = y+dy[i];
+    if (school[xx][yy] == 'P' && !mark[xx][yy]) flood(xx, yy);
+  }
+}
+
+void solve() {
+  int n, m; cin >> m >> n;
+  rep(i, 1, n) rep(j, 1, m) cin >> school[i][j];
+  int mx = 0, mn = INF;
+  rep(i, 1, n) rep(j, 1, m) {
+    if (school[i][j] == 'T') continue;
+    dp[i][j] = min({dp[i-1][j], dp[i-1][j-1], dp[i][j-1]})+1;
+    ckmax(mx, dp[i][j]);
+  }
+  rep(i, 1, n) rep(j, 1, m) qsT[i][j] = (school[i][j] == 'T') + qsT[i-1][j] + qsT[i][j-1] - qsT[i-1][j-1];
+  rep(i, mx, n) rep(j, mx, m) {
+    if (qsT[i][j] - qsT[i-mx][j] - qsT[i][j-mx] + qsT[i-mx][j-mx]) continue;
+    int cnt = 0; memset(mark, 0, sizeof(mark));
+    rep(x, i-mx+1, i) rep(y, j-mx+1, j) {
+      if (school[x][y] == 'P' && !mark[x][y]) cnt++, flood(x, y);  
+    }
+    ckmin(mn, cnt);
+  }
+  cout << mx*mx << " " << mn << nl;
+}  
+
+int main(int argc, char* argv[]) {
+  ios_base::sync_with_stdio(0); cin.tie(NULL);
+  int t = 1;
+//  cin >> t;
+  while (t--) { solve(); }
+  return 0;
+}
