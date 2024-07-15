b422cb1fa55f9ba58cc19d6b69e53a3d7bc1c76b
spad1e <pasin.jechou@gmail.com>
2024-07-15 07:37:06 +0700

    cf1989: A-E

---
+++ codeforces/cf1989/E.cpp
@@ -0,0 +1,84 @@
+#pragma once
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef double db;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<ld, ld> pld;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<ld> vld;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+typedef vector<pld> vpld;
+typedef vector<vi> vvi;
+typedef tuple<int, int, int> t3;
+typedef tuple<int, int, int, int> t4;
+typedef tuple<int, int, int, int, int> t5;
+typedef tuple<ll, ll, ll> tll3;
+typedef tuple<ll, ll, ll, ll> tll4;
+typedef tuple<ll, ll, ll, ll, ll> tll5;
+
+template<typename T> using pq = priority_queue<T>;
+template<typename T> using pqg = priority_queue<T, vector<T>, greater<T>>;
+
+#define rep0(a) for (int i = 0; i < a; ++i)
+#define rep1(i, a) for (int i = 0; i < a; ++i)
+#define rep2(i, a, b) for (int i = a; i <= b; ++i)
+#define rep3(i, a, b, c) for (int i = a; i <= b; i+=c) 
+#define overload4(a, b, c, d, e, ...) e
+#define rep(...) overload4(__VA_ARGS__, rep3, rep2, rep1, rep0)(__VA_ARGS__)
+#define repd0(a) for (int i = a; i >= 1; --i)
+#define repd1(i, a) for (int i = a; i >= 1; --i)
+#define repd2(i, a, b) for (int i = b; i >= a; --i)
+#define repd3(i, a, b, c) for (int i = b; i >= a; i-=c)
+#define repd(...) overload4(__VA_ARGS__, repd3, repd2, repd1, repd0)(__VA_ARGS__)
+#define trav(a, x) for (auto& a : x)
+
+#define sz(x) (int)(x).size()
+#define mp make_pair
+#define pb push_back
+#define st first
+#define nd second
+#define lb lower_bound
+#define ub upper_bound
+#define all(x) (x).begin(), (x).end()
+#define ins insert
+
+template<typename T> bool ckmin(T &a, const T &b) { return b < a ? a = b, 1 : 0; }
+template<typename T> bool ckmax(T &a, const T &b) { return a < b ? a = b, 1 : 0; }
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+const int MOD = 998244353;
+const int INF = 0x3fffffff;
+const ll LINF = 0x1fffffffffffffff;
+const char nl = '\n';
+const int MX = 2e5 + 3;
+
+ll dp[MX][11], sum[MX][11];
+
+void solve() {
+  int n, k; cin >> n >> k;
+  rep(i, 0, n) sum[i][0] = 1;
+  rep(i, 1, n) {
+    rep(j, 1, k) {
+      if (j==k) dp[i][j] = (dp[i][j] + sum[i-1][j-1] + sum[i-1][j] - (i>1&&i!=n&&j!=1?dp[i-2][j-1]+dp[i-2][j]:0)) % MOD; 
+      else dp[i][j] = (((dp[i][j] + sum[i-1][j-1] - (i>1&&i!=n&&j!=1?dp[i-2][j-1]:0)) % MOD) + MOD) % MOD;
+    }
+    rep(j, 1, k) sum[i][j] = (sum[i-1][j] + dp[i][j]) % MOD;
+  }
+  cout << dp[n][k] << nl;
+}
+
+
+int main(int argc, char* argv[]) {
+  ios_base::sync_with_stdio(0); cin.tie(NULL);
+  int t = 1;
+  // cin >> t;
+  while (t--) { solve(); }
+  return 0;
+}
