38188a80436986a440ed16149d9b3573986cb9ef
Pasin Pornsiwakul <pasinpornsiwakul@spad1e.local>
2025-06-07 05:11:36 +0700

    fix: optimize time

--- Olympiads/TOI/TOI20_orbs/orbs.cpp
+++ Olympiads/TOI/TOI20_orbs/orbs.cpp
@@ -56,39 +56,43 @@ const ll LINF = 0x1fffffffffffffff;
 const char nl = '\n';
 const int MX = 2e6 + 10;
 
-multiset<int> ms1, ms2, ms3;
+priority_queue<int> pqL;
+priority_queue<int, vector<int>, greater<int>> pqR;
+multiset<int> ms;
 int x[MX];
 
 void solve() {
   int n, l, a, b; cin >> n >> l >> a >> b;
   rep(i, 1, n) cin >> x[i];
   sort(x+1, x+n+1);
-  rep(i, 1, a-1) ms1.ins(x[i]);
-  rep(i, a, b-1) ms2.ins(x[i]);
-  rep(i, b, n) ms3.ins(x[i]);
+  rep(i, 1, a-1) pqL.push(x[i]);
+  rep(i, a, b-1) ms.ins(x[i]);
+  rep(i, b, n) pqR.push(x[i]);
   while (l--) {
-    int x = *ms2.begin();
-    int y = *ms3.begin();
-    ms2.erase(ms2.begin());
-    ms3.erase(ms3.begin());
+    int x = *ms.begin();
+    int y = pqR.top();
+    ms.erase(ms.begin());
+    pqR.pop();
 
-    if (ms2.empty() || y-x < *ms2.begin()) {
-      ms1.ins(y-x);
-      ms2.ins(*(--ms1.end()));
-      ms1.erase(--ms1.end());
+    if (ms.empty() || y-x < *ms.begin()) {
+      pqL.push(y-x);
+      ms.ins(pqL.top());
+      pqL.pop();
     }
-    else ms2.ins(y-x);
+    else ms.ins(y-x);
     int z = (x+y)/2;
-    if (ms3.empty() || z < *ms3.begin()) {
-      ms2.ins(z);
-      ms3.ins(*(--ms2.end()));
-      ms2.erase(--ms2.end());
+    if (!pqR.empty() || z < pqR.top()) {
+      ms.ins(z);
+      pqR.push(*(--ms.end()));
+      ms.erase(--ms.end());
     }
-    else ms3.ins(z);
+    else pqR.push(z);
   }
-  for (auto x : ms1) cout << x << " ";
-  for (auto x : ms2) cout << x << " ";
-  for (auto x : ms3) cout << x << " "; 
+  stack<int> s;
+  while (!pqL.empty()) s.push(pqL.top()), pqL.pop();
+  while (!s.empty()) cout << s.top() << " ", s.pop();
+  for (auto x : ms) cout << x << " ";
+  while (!pqR.empty()) cout << pqR.top() << " ", pqR.pop();
   cout << nl;
 }  
 
